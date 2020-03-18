fc0e3f8b04c404ae37ec0c5daef59fb10082ba85
Avi Yaniger <avi.yaniger@gmail.com>
2020-03-18 11:20:58 +0200

    finished project

--- script.js
+++ script.js
@@ -1,62 +1,93 @@
-const operate = (a, b, operation) => {
-    if (operation === ' + ') {
-        return a + b;
-    } else if (operation === ' - ') {
-        return a - b;
-    } else if (operation === ' * ') {
-        return a * b;
-    } else {
-        return a / b;
-    }
-};
+function isOperator(x) {
+    if ((x === "^") || (x === "*") || (x === "/") || (x === "+") || (x === "-")) {
+        return true;
+    } else return false;
+}
 
-//var p = document.querySelector('p');
-//var currentNum = '';
-//var num1;
-//var num2;
-//var operation = '';
-//var operators = document.querySelectorAll('.operator');
-//operators.forEach(function(operator){
-//    operator.addEventListener('click', function(){
-//        if (!operation === ''){
-//            num2 = currentNum;
-//            var a = parseFloat(num1);
-//            var b = parseFloat(num2);
-//            currentNum = operate(a, b, operation);
-//        }
-//        operation = operator.textContent;
-//        num1 = currentNum;
-//        currentNum = '';
-//        p.textContent += operator.textContent;
-//    });
-//})
-//var numbers = document.querySelectorAll('.numbers');
-//numbers.forEach(function(number){
-//    number.addEventListener('click', function(){
-//        currentNum += number.textContent;
-//        p.textContent += number.textContent;
-//    });
-//});
-//var equals = document.querySelector('#equals');
-//equals.addEventListener('click', function(){
-//    num2 = currentNum;
-//    var a = parseFloat(num1);
-//    var b = parseFloat(num2);
-//    if (b === 0 && operation === ' / ') {
-//        p.textContent = 'ERROR';
-//    } else {
-//        p.textContent = operate(a, b, operation);
-//        currentNum = p.textContent;
-//        operation = '';
-//    }
-//});
-//var clear = document.querySelector('#clear');
-//clear.addEventListener('click', function(){
-//    p.textContent = '';
-//    currentNum = '';
-//    operation = '';
-//});
-//var backspace = document.querySelector('#backspace');
-//backspace.addEventListener('click', function(){
-//    
-//});
+var p = document.querySelector("p");
+var solved = false;
+
+var numbers = document.querySelectorAll(".number");
+numbers.forEach((number) => {
+    number.addEventListener("click", function() {
+        if (solved) {p.textContent = ""};
+        solved = false;
+        p.textContent += number.textContent;
+    })
+})
+
+var operators = document.querySelectorAll(".operator");
+operators.forEach((operator) => {
+    operator.addEventListener("click", function() {
+        solved = false;
+        p.textContent += operator.textContent;
+    })
+})
+
+var clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+    p.textContent = ""
+    solved = false;
+});
+
+var backspace = document.querySelector("#backspace");
+backspace.addEventListener("click", () => {
+    p.textContent = p.textContent.slice(0, -1);
+    solved = false;
+})
+
+
+var equals = document.querySelector("#equals");
+equals.addEventListener("click", function() {
+    
+    // Create problem array
+    const charArr = p.textContent.split("");
+    let problem = [];
+    let tmp = "";
+    var answer;
+    for (let i = 0; i < charArr.length; i++) {
+        if (!isOperator(charArr[i])) {
+            tmp += charArr[i];
+        } else {
+            problem.push(tmp);
+            problem.push(charArr[i]);
+            tmp = "";
+        }
+    }
+    // Add last number to problem array
+    problem.push(tmp);
+
+    // Look for ^
+    for (let i = 0; i < problem.length; i++) {
+        if (problem[i] === "^") {
+            answer = parseFloat(problem[i-1]) ** parseFloat(problem[i+1]);
+            problem.splice(i-1, 3, answer);
+        }
+    }
+
+    // Look for * or /
+    for (let i = 0; i < problem.length; i++) {
+        if (problem[i] === "*") {
+            answer = parseFloat(problem[i-1]) * parseFloat(problem[i+1]);
+            problem.splice(i-1, 3, answer);
+        } else if (problem[i] === "/") {
+            answer = parseFloat(problem[i-1]) / parseFloat(problem[i+1]);
+            problem.splice(i-1, 3, answer);
+        }
+    }
+
+    // Look for + or -
+    for (let i = 0; i < problem.length; i++) {
+        if (problem[i] === "+") {
+            answer = parseFloat(problem[i-1]) + parseFloat(problem[i+1]);
+            problem.splice(i-1, 3, answer);
+        } else if (problem[i] === "-") {
+            answer = parseFloat(problem[i-1]) - parseFloat(problem[i+1]);
+            problem.splice(i-1, 3, answer);
+        }
+    }
+
+    p.textContent = answer;
+    solved = true;
+
+});
